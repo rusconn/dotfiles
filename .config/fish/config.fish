e7c50aadb2ba447d0bd86d19fa916b79f0a145b7
rusconn <rusconn3@gmail.com>
2023-05-29 12:57:37 +0900

    Add volta settings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,6 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
+if status is-login
+    # volta
+    set -gx VOLTA_HOME "$HOME/.volta"
+    set -gx PATH "$VOLTA_HOME/bin" $PATH
+    set VOLTA_FEATURE_PNPM 1
 end
