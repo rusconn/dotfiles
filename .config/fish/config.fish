6da23b14d44941f74e368bf30bed2901d0a89265
rusconn <rusconn3@gmail.com>
2023-06-17 11:06:30 +0900

    Add OrbStack command-line tools path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,6 +9,9 @@ if status is-login
 
     # google-cloud-sdk
     source "$(brew --prefix)/share/google-cloud-sdk/path.fish.inc"
+
+    # Added by OrbStack: command-line tools and integration
+    source ~/.orbstack/shell/init.fish 2>/dev/null || :
 end
 
 # mise
