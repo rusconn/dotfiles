f5d6894dd37094003b6fce603a9f9634c30e5a03
rusconn <rusconn3@gmail.com>
2024-02-06 18:43:40 +0900

    Add rust path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,9 @@
 if status is-login
+    # Rust
+    string match -q -r ".*:$HOME/.cargo/bin:.*" $PATH
+    if not test $status -eq 0
+        set -x PATH $HOME/.cargo/bin $PATH
+    end
 end
 
 # mise
